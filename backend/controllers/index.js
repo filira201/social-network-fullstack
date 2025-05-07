85001c6c21349de35695620df88ebd15c6a7a598
Egor <egor297198@mail.ru>
2025-05-07 18:34:34 +0400

    add comment controller

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,4 +1,5 @@
 const UserController = require("./userController");
 const PostController = require("./postController");
+const CommentController = require("./commentController");
 
-module.exports = { UserController, PostController };
+module.exports = { UserController, PostController, CommentController };
