435e1f282f26feb1806b4e167693ccdd013c07b9
Egor <egor297198@mail.ru>
2025-05-10 17:55:59 +0400

    new theme toggler

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,10 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Switch,
+} from "@heroui/react";
 import { useGetTheme } from "../contexts/theme/useGetTheme";
 import { FaRegMoon } from "react-icons/fa";
 import { LuSunMedium } from "react-icons/lu";
@@ -12,12 +18,14 @@ const Header = () => {
         <p className="font-bold text-inherit">Social Network</p>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem
-          onClick={toggleTheme}
-          className="text-3xl cursor-pointer lg:flex"
-        >
-          {!darkMode ? <FaRegMoon /> : <LuSunMedium />}
-        </NavbarItem>
+        <Switch
+          isSelected={!darkMode}
+          onValueChange={toggleTheme}
+          endContent={<LuSunMedium />}
+          startContent={<FaRegMoon />}
+          size="lg"
+        ></Switch>
+
         <NavbarItem></NavbarItem>
       </NavbarContent>
     </Navbar>
